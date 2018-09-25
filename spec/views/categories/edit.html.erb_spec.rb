64bbf0e7e2419110ba90c17eb6823926decb504d
github username <github email address>
2018-09-25 15:35:38 -0400

    associations within forms with rails.

--- spec/views/categories/edit.html.erb_spec.rb
+++ spec/views/categories/edit.html.erb_spec.rb
@@ -5,9 +5,7 @@ RSpec.describe "categories/edit", type: :feature do
     visit edit_category_path(@category)
 
     fill_in 'category[name]', with: "My Edited Category"
-
     click_on "Update Category"
-
     expect(current_path).to eq(category_path(@category))
     expect(page).to have_content("My Edited Category")
   end
